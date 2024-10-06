1bd83cef321f9b2bc0da062aebad84e09bb1e0e1
Guilherme Castilho dos Santos <172083299+guicastilhodev@users.noreply.github.com>
2024-10-06 19:45:28 -0300

    send index.html file

--- myApp.js
+++ myApp.js
@@ -1,9 +1,11 @@
 let express = require('express');
 let app = express();
+let absolutePath = __dirname + '/views/index.html';
 console.log("Hello World");
 
 app.get("/", function (req, res) {
-    res.send("Hello Express");
+    
+    res.sendFile(absolutePath);
 }); 
 
 
