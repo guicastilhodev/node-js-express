b476f3680f65de936d7d61263b803a49ee67446a
Guilherme Castilho dos Santos <172083299+guicastilhodev@users.noreply.github.com>
2024-10-07 10:11:02 -0300

    add var MESSAGE_STYLE

--- myApp.js
+++ myApp.js
@@ -1,20 +1,26 @@
+require('dotenv').config();
 let express = require('express');
 let app = express();
 const path = require('path');
+const messageStyle = process.env.MESSAGE_STYLE;
 
 let absolutePath = __dirname + '/views/index.html';
 let publicAbsolutePath = path.join(__dirname, '/public');
 
-
-app.use("/public", express.static(publicAbsolutePath));
+app.use(express.static(publicAbsolutePath));
 
 app.get("/", function (req, res) {
     res.sendFile(absolutePath);
 }); 
 
 app.get ("/json", function (req, res) {
-    res.json({message: "Hello json"})
-})
+    
+    if(messageStyle == "uppercase") {
+        res.json({message: "HELLO JSON"});
+    } else {
+        res.json({message: "Hello json"});
+    }
+});
 
 
 
